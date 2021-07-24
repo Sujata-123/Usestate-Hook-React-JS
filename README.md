679f89f813f34a745d7fe4ae59a4859394c94c6f
Sujata-123 <sujata672singh@gmail.com>
2021-07-24 21:13:24 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-1qcscq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-1qcscq)
---
+++ src/CounterUsingHandler.js
@@ -0,0 +1,20 @@
+import React from 'react';
+const CounterUsingHandler=()=>{
+  const handlerIncrement=()=>{
+  
+  }
+  const handlerConstant=()=>{
+
+  }
+  const handlerIncrement=()=>{
+
+  }
+  return(
+    <div>
+      <button onClick={handlerIncrement}>Increment</button>
+      <button onClick={handlerConstant}>Constant</button>
+      <button onClick={handlerIncrement}>Decrement</button>
+    </div>
+  )
+}
+export default CounterUsingHandler;
