d27134fa4be0ae9b724c315bf27f70aa1bf9ee0c
Sujata-123 <sujata672singh@gmail.com>
2021-07-24 21:26:19 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 import Counter from './Counter';
 import CounterUsingHandler from './CounterUsingHandler';
 export default function App() {
   return (
     <div>
-   <Counter/>
-      
+      <Counter />
+      <CounterUsingHandler />
     </div>
   );
 }
--- src/CounterUsingHandler.js
+++ src/CounterUsingHandler.js
@@ -1,20 +1,22 @@
-import React from 'react';
-const CounterUsingHandler=()=>{
-  const handlerIncrement=()=>{
-  
-  }
-  const handlerConstant=()=>{
-
-  }
-  const handlerIncrement=()=>{
-
-  }
-  return(
+import React, { useState } from 'react';
+const CounterUsingHandler = () => {
+  const [count1, setCount1] = useState(0);
+  const handlerIncrement = () => {
+    setCount1(count1 + 1);
+  };
+  const handlerConstant = () => {
+    setCount1(0);
+  };
+  const handlerDecrement = () => {
+    setCount1(count1 - 1);
+  };
+  return (
     <div>
+      The count is :{count1}
       <button onClick={handlerIncrement}>Increment</button>
       <button onClick={handlerConstant}>Constant</button>
-      <button onClick={handlerIncrement}>Decrement</button>
+      <button onClick={handlerDecrement}>Decrement</button>
     </div>
-  )
-}
+  );
+};
 export default CounterUsingHandler;
